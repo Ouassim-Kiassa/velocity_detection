687565f35559f1f6223d37a2cb0724ecd571c522
Ouassim Kiassa <ouassim.kiassa@outlook.com>
2025-08-02 14:10:53 +0200

    made change to camera setting

--- opyflow_LSPIV.py
+++ opyflow_LSPIV.py
@@ -12,10 +12,6 @@ import opyf
 # Loading video and set parameters
 
 video = opyf.videoAnalyzer("video.mp4")
-video.set_vecTime(Ntot=25,starting_frame=200)
-
-video.set_interpolationParams(Sharpness=2)
-video.set_goodFeaturesToTrackParams(qualityLevel=0.01)
 
 #cv2.imwrite("mask.png",video.vis)
 
@@ -23,7 +19,7 @@ video.set_goodFeaturesToTrackParams(qualityLevel=0.01)
 
 mask=cv2.imread('mask.png')
 A=mask>100
-video.set_stabilization(mask=A[:,:,0],mute=False)
+video.set_stabilization(mask=A[:,:,0],mute=True)
 
 
 ## Orthorectification
@@ -31,36 +27,45 @@ video.set_stabilization(mask=A[:,:,0],mute=False)
 image_points = np.array([
 	(250, 1470),   # left
 	(2161, 1180),   # right
-	(130, 620),  # left front
+	(130, 630),  # left front
 	(1700, 200),   # right front
                             ], dtype="double")
 
 # coordinates
 model_points = np.array([
-                    (224, 207, 0) ,#left 
-                    (246, 221, 0),# right bank
-                    (232, 213, 0),    # left front bank
-                    (252, 218, 0),], dtype="double")  # riĝht front bank
+                    (0, 180, 0) ,#left 
+                    (0, 0, 0),# right bank
+                    (91, 188, 0),    # left front bank
+                    (93, 21, 0),], dtype="double")  # riĝht front bank
 
 
-abs_or=model_points[0]
-model_points = model_points - model_points[0]
+abs_or=model_points[1]
+model_points = model_points - model_points[1]
 
 
 video.set_birdEyeViewProcessing(image_points,
-                                model_points, [1, 1, -30.],
-                                rotation=np.array([[1., 0, 0],[0,-1,0],[0,0,-1.]]),
-                                scale=True,framesPerSecond=30)
+                                model_points, [26, 52, 164.],
+                                rotation=np.array([[0., 0.5, 0],
+                                                   [1, 0, 0],
+                                                   [0, 0, 1]]),
+                                scale=True, framesPerSecond=30, saveOutPut="birdeye")
 
+
+
+
+video.set_vecTime(Ntot=10, shift=1, starting_frame=0)
+video.set_vlim([0, 1])
+
+
+video.set_filtersParams(maxDevInRadius=1, RadiusF=0.03, CLAHE = True)
+video.set_goodFeaturesToTrackParams(qualityLevel=0.01)
+video.extractGoodFeaturesDisplacementsAccumulateAndInterpolate(display1='points',display2='field',displayColor=True)
+
+
+
+
+video.opyfDisp.plotField(video.Field, vis=video.vis)
+video.showXV(video.Xaccu, video.Vaccu)
 
 print('Processing started...')
-
-
-video.set_vlim([0, 10])
-
-video.extractGoodFeaturesDisplacementsAccumulateAndInterpolate(display1='quiver',display2='field',displayColor=True)
-video.set_filtersParams(maxDevInRadius=1.5, RadiusF=0.15,range_Vx=[0.01,10])
-
-video.filterAndInterpolate()
-
-print('Processing started...')
+print('Processing started...2')
